d474c31e7a417f0372b3d7630108f7631e1d107c
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-03-10 15:08:54 +0530

    first commit -simple websocket chat

---
+++ assets/js/app.js
@@ -0,0 +1,62 @@
+$(function () {
+    var socket = io.connect();
+
+    //Message Form
+    var $chatMessageContainer = $('#chatMessageContainer');
+    var $messageForm = $('#messageForm');
+    var $messageTxt = $('#messageTxt');
+    var $conversationMessages = $('#conversationMessages');
+
+    //User Login Form
+    var $loginFormContainer = $('#loginFormContainer');
+    var $loginForm = $('#loginForm');
+    var $onlineUsers = $('#onlineUsers');
+    var $username = $('#username');
+
+    $messageForm.submit(sendChat);
+
+    $messageTxt.on('keyup', function (e) {
+        if (e.which == 13 || e.keyCode == 13) {
+            //code to execute here
+            console.log(e);            
+            sendChat(e);
+            return false;
+        }
+        return true;
+    });
+
+    function sendChat(e) {
+        e.preventDefault();
+        //console.log('Send Clicked');
+        socket.emit('send message', $messageTxt.val());
+        $messageTxt.val('');
+    }
+
+    socket.on('new message', function (data) {
+        var html = '';
+        html += '<div class="well">';
+        html += '<strong>' + data.userName + ': </strong>' + data.msgTxt;
+        html += '</div>';
+        $conversationMessages.append(html);
+    });
+
+    $loginForm.submit(function (e) {
+        e.preventDefault();
+        socket.emit('new user', $username.val(), function (data) {
+            if (data) {
+                $loginFormContainer.hide();
+                $chatMessageContainer.show();
+            }
+        });
+        $username.val('');
+    });
+
+    socket.on('get users', function (data) {
+        var html = '';
+        for (i = 0; i < data.length; i++) {
+            html += '<li class="list-group-item">' + data[i] + '</li>'
+        }
+        $onlineUsers.html(html);
+    })
+
+});
